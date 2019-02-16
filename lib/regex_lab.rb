4db2d894119a660c261cb5ebe79e693f0fda24fa
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-16 20:28:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,11 +16,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/\A[A-Z]\w+[.?!,]\b/)
+  if text.match(/\A[A-Z]\w+[.?!,]\b/)
     true
-  elsif text.scan(/\A[A-Z]\w+[^.?!,]\b/)
+  elsif text.match(/\A[A-Z]\w+[^.?!,]\b/)
     false 
-  elsif text.match(/\A[^A-Z]\w+[.?!,]\b/)
+  elsif text.match(/\A[a-z]\w+[.?!,]\b/)
     false
   elsif text.match(/\A[a-z]\w+[^.?!,]\b/)
     false 
