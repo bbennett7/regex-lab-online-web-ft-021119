58c135913a10361ecbd9a85d4069e155a4e22d0d
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-16 17:31:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
   if word.scan(/^[aeiou]/)
     true 
-  elsif #word.scan(/^[bcdfghjklmnpqrstvwxyz]/)
+  else #word.scan(/^[bcdfghjklmnpqrstvwxyz]/)
     false 
   end
 end
