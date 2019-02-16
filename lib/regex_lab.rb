63f5801448602af60a59f1c52d47bc06d4dabfa7
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-16 17:17:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/^\[aeiou]/)
+    true 
+  elsif word.scan(/^\[b-z^eiou]/)
+    false 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/^\[unUn]/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
